6df2b7f27e603956b3e05880dc57986b14651ce7
ikeda <daikenkai3542@gmail.com>
2022-12-26 02:02:18 +0900

    Prepare for publish

--- buildZip.sh
+++ buildZip.sh
@@ -7,5 +7,14 @@ npm run build
 
 # Generate zip
 cp manifest.json dist
-sed -i s/dist/./ dist/manifest.json
+cp icons/*.png dist
+rm dist/hot-reload.js
+
+TMP_MANIFEST=dist/tmp_manifest.json
+cp dist/manifest.json $TMP_MANIFEST
+sed -i "s#dist/#./#" $TMP_MANIFEST
+sed -i "s#icons/#./#" $TMP_MANIFEST
+cat $TMP_MANIFEST | jq 'del(.background)' > dist/manifest.json
+rm $TMP_MANIFEST
+
 zip build.zip -r dist
